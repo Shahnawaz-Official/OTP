d1b52f3e14bbe47bd6a826022c619b689842d0cb
shahnawaz <mdshahnawaz.420bca@gmail.com>
2026-03-28 20:16:37 +0530

    handle accesstoken and refreshtoken

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,25 +1,29 @@
+
 const User = require("../models/user.model")
 const crypto = require("crypto") // nodejs ka building Package
 const jwt = require("jsonwebtoken");
 
 
+/**
+ * @name  handleUserRegister
+ * @description user register
+ */
+
+async function handleUserRegister(req, res) {
 
 
-async function handleUserRegister (req,res){
-
-
-    const {username ,email ,password} = req.body;
+    const { username, email, password } = req.body;
 
     const isAlreadyExsist = await User.findOne({
-        $or:[
-            {username},
-            {email}
+        $or: [
+            { username },
+            { email }
         ]
     })
 
-    if(isAlreadyExsist){
+    if (isAlreadyExsist) {
         return res.status(409).json({
-            message:"Username & Email Already Exsist  "
+            message: "Username & Email Already Exsist  "
         })
     }
 
@@ -31,24 +35,35 @@ async function handleUserRegister (req,res){
     const user = await User.create({
         username,
         email,
-        password:hashPassword
+        password: hashPassword
     })
 
-    const token = jwt.sign({
-        id :user._id
-    },process.env.JWT_SECRET_KEY,{
-        expiresIn:"1d"
+    const accessToken = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "15m"  // max to max expire time 15m
     })
-   
-    
+    const refreshToken = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "7d" // max to max expire time 7d
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 100  // 7 Days
+    })
+
     res.status(201).json({
-        message:"User Register Successfully ",
-        user:{
-           id:user._id,
-           name:user.username,
-           email:user.email
+        message: "User Register Successfully ",
+        user: {
+            id: user._id,
+            name: user.username,
+            email: user.email
         },
-        token
+        accessToken
     })
 
 
@@ -56,9 +71,46 @@ async function handleUserRegister (req,res){
 }
 
 
+/**
+ * @name handleUserGetMe
+ * @description user presonal data get  
+ */
 
+async function handleUserGetMe(req, res) {
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    // console.log(token)
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Token Not Found And Unauthorized"
+        })
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    // console.log(decoded)
+
+    const user = await User.findById(decoded.id)
+
+    res.status(200).json({
+        message: "User Fatch Successfully",
+        user: {
+            username: user.username,
+            email: user.email
+        }
+    })
+
+
+}
+
+async function handleUserRefreshToken (req,res){
+
+}
 
 
 module.exports = {
-    handleUserRegister
+    handleUserRegister,
+    handleUserGetMe,
+    handleUserRefreshToken
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,24 @@
 const express = require("express")
 const router = express.Router()
 
-const {handleUserRegister} = require("../controller/auth.controller")
+const {handleUserRegister,handleUserGetMe,handleUserRefreshToken} = require("../controller/auth.controller")
 
 
-
+/**
+ * @name  POST  - /api/auth/register
+ */
 
 router.post("/register",handleUserRegister)
 
+/**
+ * @name  GET - /api/auth/get-me
+ */
+router.get("/get-me",handleUserGetMe)
 
+/**
+ *  @name  GET  - api/auth/refresh-token
+ */
+router.get("/refresh-token",handleUserRefreshToken)
 
 
 
